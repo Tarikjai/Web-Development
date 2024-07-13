3b227a917ea64f982ab720fda33d468dc352d901
Tarik J <tarik.jaidani@gmail.com>
2024-07-13 12:43:23 +0200

    adding filter by type

--- Section 30_BuiId_Your_Own_API/6.0 DIY API/index.js
+++ Section 30_BuiId_Your_Own_API/6.0 DIY API/index.js
@@ -15,13 +15,21 @@ app.get("/random" ,(req, res) => {
 
 //2. GET a specific joke
 
-app.get("jokes/:id", (req, res) => {
+app.get("/jokes/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const foundJoke = jokes.find((joke) => joke.id === id)
   res.json(foundJoke);
 })
 
+
 //3. GET a jokes by filtering on the joke type
+
+app.get("/filter", (req, res)=> {
+  const type = req.query.type;
+  const filteredActivities = jokes.filter((joke) => joke.jokeType === type)
+  res.json(filteredActivities);
+})
+ 
 
 //4. POST a new joke
 
