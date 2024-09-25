a7e9bea4f6192bf37497c4aa8fa2a02e8a70dddc
Tarik <tarik.jaidani@gmail.com>
2024-09-25 11:53:05 +0200

    node

--- Section_23_Using Node/2.2_Native_Modules/index.js
+++ Section_23_Using Node/2.2_Native_Modules/index.js
@@ -1,6 +1,8 @@
- const fs = require('fs')
- const path = require('path')
-/*
+const fs = require('fs')
+const path = require('path')
+ 
+
+
  fs.writeFile(path.join(__dirname,'message.txt') ,'Hello my name is Tarik and I am a web developper', err => {
     if(err){
       console.log(err)
@@ -8,9 +10,9 @@
       console.log("message created")
     }
  })
-*/
 
 
+/*
 fs.readFile(path.join(__dirname,'message.txt'), 'utf8', (err, data) => {
   if (err) {
     console.error(err);
@@ -18,3 +20,4 @@ fs.readFile(path.join(__dirname,'message.txt'), 'utf8', (err, data) => {
   }
   console.log(data);
 });
+*/
