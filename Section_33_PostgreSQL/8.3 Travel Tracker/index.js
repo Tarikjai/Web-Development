8df298bccb447dc0ff7629f31afb3cc2317157ba
Tarik <tarik.jaidani@gmail.com>
2024-10-10 16:58:34 +0200

    Travel Tracker

--- Section_33_PostgreSQL/8.3 Travel Tracker/index.js
+++ Section_33_PostgreSQL/8.3 Travel Tracker/index.js
@@ -1,41 +1,55 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from 'pg'
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "6598430",
+  port: 5432,
+});
+db.connect();
+
+const checkVisisted = async()=>{
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let result
-let total 
-const db = new pg.Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'world',
-  password: '6598430',
-  port: 5432,
-})
-
-db.connect()
-
-db.query("select * from visited_countries",(err,res)=>{
-  if (err) {
-    console.error("Error executing query ", err.stack)
-  } else {
-    result = res.rows
-  }
-  db.end()
-  console.log(result.map(obj =>   obj.country_code) )
-})
-
+// GET home page
 app.get("/", async (req, res) => {
-  //Write your code here.
-  const countries = result.map(obj =>   obj.country_code)
-  const total = countries.length
-  res.render('index.ejs', { countries ,total })
+  const countries = await checkVisisted()
+  res.render("index.ejs", { countries: countries, total: countries.length });
 });
+
+
+app.post('/add', async (req,res)=>{
+  const input = req.body.country
+
+  const result = await db.query(`SELECT country_code FROM countries where country_name = $1`,[input]);
+  const countryCode = result.rows[0].country_code 
+ 
+  if (countryCode){
+   // const result = await db.query(`SELECT country_name FROM countries where country_code = $1`,[countryCode]);
+   // const countryName = result.rows[0].country_name
+    await db.query(`INSERT INTO visited_countries (country_code) VALUES ($1)`,[countryCode])
+    /*const countries = await checkVisisted()
+    res.render("index.ejs", { countries: countries, total: countries.length });*/
+    res.redirect("/")
+  }
+  
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
